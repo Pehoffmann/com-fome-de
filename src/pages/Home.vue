6c3db569fbb21ef9f23031083471fee93c6bdee8
Pedro Hoffmann <pedrob_hoffmann@outlook.com>
2023-06-01 17:22:43 -0300

    #5 - feat: problemas com cors na api places

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <NavBar />
     <UserLocationForm />
+    <LocalsList />
   </div>
 </template>
 
 <script>
 import UserLocationForm from "../components/UserLocationForm.vue";
 import NavBar from "../components/NavBar.vue";
+import LocalsList from "../components/LocalsList.vue";
 export default {
   name: "Home",
   components: {
     UserLocationForm,
-    NavBar
+    NavBar,
+    LocalsList
   },
   data() {
     return {};
